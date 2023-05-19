364c43b194d86b6532086f91f57dbb5fdb96309f
Olena1986 <125265067+Olena1986@users.noreply.github.com>
2023-05-19 11:10:58 +0200

    f

--- src/components/feedback/FeedbackOptions.jsx
+++
@@ -1,9 +0,0 @@
-import { FeedbackStyle } from './feedback.styled.js' 
-
-export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
-  <FeedbackStyle.StyleBtnContainer>
-    <FeedbackStyle.StyleButton onClick={() => onLeaveFeedback('good')}>Good</FeedbackStyle.StyleButton>
-    <FeedbackStyle.StyleButton onClick={() => onLeaveFeedback('neutral')}>Neutral</FeedbackStyle.StyleButton>
-    <FeedbackStyle.StyleButton onClick={() => onLeaveFeedback('bad')}>Bad</FeedbackStyle.StyleButton>
-  </FeedbackStyle.StyleBtnContainer>
-);
